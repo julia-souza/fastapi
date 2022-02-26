7920f16f60776decb029e52f8be4a8e15fe5d0a8
Julia Souza <juliadsouzaborges@gmail.com>
2022-02-26 08:54:31 -0500

    second commit

--- src/model/pedido.py
+++ src/model/pedido.py
@@ -10,22 +10,48 @@ class Pedido(Entity):
     quantity: int
     client: str
     status: bool
+
     def classname(self):
         return 'pedido'
+
     def check(self):
+
+        
+
         read = crud.read(self.isbn, "book")
 
         read = json.loads(read)
         
         if(read['quantity'] >= self.quantity):
             self.status = True  #pedido aprovado!
-            read['quantity']-self.quantity
-            pedido = Pedido(id_pedido=dict['id_pedido'],isbn=dict['isbn'],quantity=dict['quantity'],client=dict['client'], status=dict['status']) 
+            qtd = read['quantity']-self.quantity
+            
+
+            dados_pedido['id'] = read['isbn']
+            print(dados_pedido['id'])
+           
+
+            quantity = qtd
+            reserved = self.quantity
+            id_pedido = self.id_pedido
+            client = self.client
+            status = True
+
+            #json_obj = json.dumps(read, indent = id)
+            #print(json_obj)
+        
+            pedido = Pedido(id_pedido=id_pedido,isbn=id,quantity=quantity,client=client, status=status) 
+            print(pedido)
+            
+            
+            #pedido = Pedido(id_pedido=dict['id_pedido'],isbn=dict['isbn'],quantity=dict['quantity'],client=dict['client'], status=dict['status']) 
             #id_pedido=dict['id_pedido'],isbn=dict['isbn'],quantity=dict['quantity'],client=dict['client'], status=dict['status']
             #crud.update(pedido, 'book')
+        
         else:
             self.status = False #reserva
         return True
+    
     def keygen(self):        
         return f"{self.classname()}/{self.id_pedido}"
          
