f2081e7cca57f32844be3d790d26f1c18e9f73ac
Julia Souza <juliadsouzaborges@gmail.com>
2022-02-26 10:14:29 -0500

    encode pedido

--- src/model/pedido.py
+++ src/model/pedido.py
@@ -3,6 +3,7 @@ from exceptions.custom import BadRequestException, NotFoundException
 from model.entity import Entity
 import dao.crud as crud
 import json
+import sys
 
 class Pedido(Entity):   
     id_pedido: int 
@@ -16,8 +17,6 @@ class Pedido(Entity):
 
     def check(self):
 
-        
-
         read = crud.read(self.isbn, "book")
 
         read = json.loads(read)
@@ -25,28 +24,15 @@ class Pedido(Entity):
         if(read['quantity'] >= self.quantity):
             self.status = True  #pedido aprovado!
             qtd = read['quantity']-self.quantity
+            dados = {'quantity':qtd, 'reserverd':self.quantity,'id_pedido':self.id_pedido, 'client':self.client,'status':True}
+            json_obj = json.dumps(dados)
             
-
-            dados_pedido['id'] = read['isbn']
-            print(dados_pedido['id'])
-           
-
-            quantity = qtd
-            reserved = self.quantity
-            id_pedido = self.id_pedido
-            client = self.client
-            status = True
-
-            #json_obj = json.dumps(read, indent = id)
-            #print(json_obj)
-        
-            pedido = Pedido(id_pedido=id_pedido,isbn=id,quantity=quantity,client=client, status=status) 
+            pedido = Pedido(id_pedido=self.id_pedido, isbn=self.isbn, quantity=qtd, client=self.client, status=self.status)
             print(pedido)
-            
-            
-            #pedido = Pedido(id_pedido=dict['id_pedido'],isbn=dict['isbn'],quantity=dict['quantity'],client=dict['client'], status=dict['status']) 
+
             #id_pedido=dict['id_pedido'],isbn=dict['isbn'],quantity=dict['quantity'],client=dict['client'], status=dict['status']
-            #crud.update(pedido, 'book')
+            
+            crud.update(pedido, 'book')
         
         else:
             self.status = False #reserva
